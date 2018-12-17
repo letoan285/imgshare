0e7490aef90efbeef0f9bda283d17156dbe7cf3b
toan le <letoan285@gmail.com>
2018-12-18 06:13:48 +0700

    initial

---
+++ src/server/config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const morgan = require('morgan');
+const multer = require('multer');
+const errorhandlers = require('errorhandler');
+
+const routes = require('../routes');
+
+module.exports = (app) => {
+    //Setting
+    app.set('port', process.env.PORT || 3001);
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+    }));
+    app.set('view engine', '.hbs');
+
+    //Midleware
+    app.use(morgan('dev'));
+    app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image'));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    //routes
+    routes(app)
+
+
+    //static file
+    app.use('/public', express.static(path.join(__dirname, '../public')));
+
+    //errorhandlers
+    if('development' === app.get('env')) {
+        app.use(errorhandlers);
+    }
+
+
+    return app;
+}
